ec9a3e44c6e451a53cfd0d476809714f389a784c
Leo Youmu <saber2pr@gmail.com>
2023-05-04 15:58:13 +0800

    Update app-printer.tsx

--- web/src/app-printer.tsx
+++ web/src/app-printer.tsx
@@ -2,13 +2,14 @@ import React, { useState } from 'react'
 import { View } from './pages/view'
 import 'normalize.css'
 import { FileUpload } from './components'
+import { isJSON } from './utils/is'
 
 export const AppPrinter = () => {
   const [view, change] = useState<JSX.Element>(
     <div>
       <FileUpload
         onUploaded={(value: string): any =>
-          change(<View data={JSON.parse(value || '{}')} />)
+          change(<View data={isJSON(value) ? JSON.parse(value) : {}} />)
         }
       />
       <p style={{ color: '#465d7f' }}>
